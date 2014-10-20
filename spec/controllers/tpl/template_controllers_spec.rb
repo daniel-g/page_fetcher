37365dffc434cb5edc44fecf79c463e1d20f8de7
Daniel Gaytan <dgaytan6@gmail.com>
2014-10-20 00:12:12 -0500

    Refactor shared examples for template controllers

---
+++ spec/controllers/tpl/template_controllers_spec.rb
@@ -0,0 +1,19 @@
+shared_examples_for 'template controller' do
+  it 'does not render any layout' do
+    actions.each do |action_name|
+      get :show, id: action_name
+      expect(response).to be_success
+      expect(response).to_not render_template /layout/
+      expect(response).to render_template action_name
+    end
+  end
+
+  it 'renders the template specified in the id' do
+    actions.each do |action_name|
+      get :show, id: action_name
+      expect(response).to be_success
+      expect(response).to render_template action_name
+    end
+  end
+end
+
